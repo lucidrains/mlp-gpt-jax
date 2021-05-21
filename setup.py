46c09e414dd1d8f3a4d2e42d36306624e06bb5e2
Phil Wang <lucidrains@gmail.com>
2021-05-21 12:51:49 -0700

    add setup.py and scaffold

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlp-gpt-jax",
+    packages=find_packages(),
+    version="0.0.1",
+    license="MIT",
+    description="MLP GPT - Jax",
+    author="Phil Wang",
+    author_email="",
+    url="https://github.com/lucidrains/mlp-gpt-jax",
+    keywords=[
+        "artificial intelligence",
+        "deep learning",
+        "language model",
+        "multilayered-perceptron",
+        "jax",
+    ],
+    install_requires=[
+        "click",
+        "click-option-group",
+        "einops>=0.3",
+        "flax",
+        "jax",
+        "jaxlib",
+        "optax",
+        "torch",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
