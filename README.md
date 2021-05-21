a2c65f1ab1dace0c6dcea76f989e2177fd08ba7a
Phil Wang <lucidrains@gmail.com>
2021-05-21 15:20:50 -0700

    complete mlp gpt architecture in flax linen

--- README.md
+++ README.md
@@ -4,6 +4,32 @@ A GPT, made only of MLPs, in Jax. The specific MLP to be used are <a href="https
 
 <a href="https://github.com/lucidrains/g-mlp-gpt">Working Pytorch implementation</a>
 
+## Install
+
+```bash
+$ pip install mlp-gpt-jax
+```
+
+## Usage
+
+```python
+from jax import random, numpy as np
+from mlp_gpt_jax import MLPGpt
+
+gpt = MLPGpt(
+    num_tokens = 20000,
+    dim = 512,
+    depth = 6,
+    seq_len = 512
+)
+
+key    = random.PRNGKey(0)
+seq    = random.randint(key, (512,), 0, 20000)
+
+params = gpt.init(key, seq)
+logits = gpt.apply(params, seq) # (512, 20000)
+```
+
 ## Citations
 
 ```bibtex
--- mlp_gpt_jax/mlp_gpt_jax.py
+++ mlp_gpt_jax/mlp_gpt_jax.py
@@ -1,6 +1,66 @@
 import jax
+from jax import numpy as np
+import jax.nn.initializers as init
+
 from flax import linen as nn
+from einops import rearrange
+
+class gMLP(nn.Module):
+    dim: int
+    seq_len: int
+    dim_ff: int
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.LayerNorm()(x)
+        x = nn.Dense(features = self.dim_ff)(x)
+        x = nn.gelu(x)
+        x = SGU(seq_len = self.seq_len)(x)
+        x = nn.Dense(features = self.dim)(x)
+        return x
+
+class SGU(nn.Module):
+    seq_len: int
+
+    def setup(self):
+        return
+
+    @nn.compact
+    def __call__(self, x):
+        n = self.seq_len
+        x, gate = np.split(x, 2, axis = -1)
+        gate = nn.LayerNorm()(gate)
+
+        weights = self.param('spatial_weights', init.uniform(scale = 1e-3 // n), (n, n))
+        bias = self.param('spatial_bias', init.ones, (n, 1))
+
+        tri_mask = np.ones((n, n))
+        causal_mask = np.triu(tri_mask, 1)
+        weights *= causal_mask
+
+        gate = np.einsum('n d, m n -> m d', gate, weights)
+        gate = gate + bias
+
+        return x * gate
 
 class MLPGpt(nn.Module):
+    num_tokens: int
+    dim: int
+    seq_len: int
+    depth: int
+    ff_mult: int = 4
+
+    def setup(self):
+        self.layers = [gMLP(dim = self.dim, dim_ff = self.dim * self.ff_mult, seq_len = self.seq_len) for _ in range(self.depth)]
+
+    @nn.compact
     def __call__(self, x):
+        to_embed = nn.Embed(num_embeddings = self.num_tokens, features = self.dim)
+        x = to_embed(x)
+
+        for layer in self.layers:
+            x = layer(x) + x
+
+        x = nn.LayerNorm()(x)
+        x = nn.Dense(features = self.num_tokens)(x)
         return x
